9a9f19237a48b6e56c7d05b234a26cba818b64f9
Flawedmatrix <flawedmatrix@gmail.com>
2024-08-01 01:42:09 -0700

    Move lib into a workspace crate

---
+++ ppo/src/lib.rs
@@ -0,0 +1,11 @@
+pub struct TrainingConfig<P: AsRef<std::path::Path>> {
+    pub model_path: P,
+    pub num_steps: usize,
+    pub num_updates: usize,
+    pub num_epochs: usize,
+    pub num_batches: usize,
+}
+
+pub fn train<T, P: AsRef<std::path::Path>>(init_state: T, config: TrainingConfig<P>) {
+    println!("Hello, world!");
+}
